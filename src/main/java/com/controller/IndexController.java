9e8118978238c2a5abd7d8c6519904814c18d119
liuyq <3449793783@qq.com>
2021-01-31 21:38:11 +0800

    new git

---
+++ src/main/java/com/controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class IndexController {
+	
+	@RequestMapping("index")
+	public Object index(){
+		return "hello word";
+	}
+}
